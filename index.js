8cc6d791a1ba21fd51e1c01d8d8d56b40a5a3d9c
Holden Altaffer <haltaffer@gmail.com>
2017-10-31 00:30:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ function prependKitten(name) {
 };
 
 function removeLastKitten() {
-  return kittens = kittens.slice(0, kittens.length - 1)
-
+kittens.slice(0, kittens.length - 1);
+return kittens
 }
